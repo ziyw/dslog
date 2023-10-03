9a33659c4df557594b3db7353f0fb924154d768e
ziyw <zywong159@gmail.com>
2023-10-02 21:48:57 -0400

    Hard code port

--- server/server.go
+++ server/server.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"flag"
 	"fmt"
 	"log"
 	"net"
@@ -13,9 +12,7 @@ import (
 	"google.golang.org/grpc"
 )
 
-var (
-	port = flag.Int("port", 50051, "The server port")
-)
+const ServerPort = "50051"
 
 type server struct {
 	pb.UnimplementedDslogServer
@@ -54,14 +51,15 @@ func (s *server) PersistLog(content string) error {
 }
 
 func main() {
-	flag.Parse()
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	lis, err := net.Listen("tcp", ServerPort)
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		log.Fatalf("server fail to listen: %v", err)
 	}
+
 	s := grpc.NewServer()
 	pb.RegisterDslogServer(s, &server{})
-	log.Printf("server listening at %v", lis.Addr())
+	log.Printf("server is listening at %v", lis.Addr())
+
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
