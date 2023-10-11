1781e709dc4fe11fe239c776bc925f40ddd98f1e
ziyw <zywong159@gmail.com>
2023-10-10 22:40:42 -0400

    Basic repo done

---
+++ server/repo.go
@@ -0,0 +1,172 @@
+// TODO:
+// - GetById()
+// - GetByType()
+// - GetByTimeRange()
+// - GetAll()
+// - GetAllAsync()
+
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"time"
+
+	_ "github.com/lib/pq"
+)
+
+type LogEntry struct {
+	id        int
+	createdAt time.Time
+	logType   string
+	logMsg    string
+}
+
+func (l LogEntry) nicePrintln() {
+	fmt.Printf("%d %s %s %s\n", l.id, l.createdAt.Format(time.RFC3339), l.logType, l.logMsg)
+}
+
+type Repo struct {
+	db *sql.DB
+}
+
+func (r *Repo) Connect() error {
+	connStr := "user=ziyan password=postgres dbname=logdb sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return err
+	}
+	r.db = db
+	return nil
+}
+
+func (r *Repo) Close() {
+	r.db.Close()
+}
+
+func (r *Repo) Insert(createdAt time.Time, logTyp, logMsg string) (int, error) {
+	sqlStmt := `
+	INSERT INTO dslog (created_at, logType, logMsg) 
+	VALUES ($1, $2, $3)
+	RETURNING id`
+
+	id := 0
+	err := r.db.QueryRow(sqlStmt, createdAt, logTyp, logMsg).Scan(&id)
+	if err != nil {
+		log.Fatal("error insert item: ", err)
+		return 0, err
+	}
+	return id, nil
+}
+
+func (r *Repo) GetById(id int) []LogEntry {
+	sqlStmt := "SELECT * FROM dslog WHERE id = $1"
+
+	rows, err := r.db.Query(sqlStmt, id)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	entries := []LogEntry{}
+	for rows.Next() {
+		var entry LogEntry
+		if err := rows.Scan(&entry.id, &entry.createdAt, &entry.logType, &entry.logMsg); err != nil {
+			log.Fatal(err)
+		}
+		entries = append(entries, entry)
+	}
+	return entries
+}
+
+func (r *Repo) GetByType(logType string) []LogEntry {
+	sql := "SELECT * FROM dslog WHERE logType = $1;"
+	rows, err := r.db.Query(sql, logType)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	entries := []LogEntry{}
+	for rows.Next() {
+		var entry LogEntry
+		if err := rows.Scan(&entry.id, &entry.createdAt, &entry.logType, &entry.logMsg); err != nil {
+			log.Fatal(err)
+		}
+		entries = append(entries, entry)
+	}
+	return entries
+}
+
+func (r *Repo) GetByTimeRange(startTime, endTime time.Time) []LogEntry {
+	sql := "SELECT * FROM dslog WHERE created_at BETWEEN $1 AND $2"
+	rows, err := r.db.Query(sql, startTime, endTime)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	entries := []LogEntry{}
+	for rows.Next() {
+		var entry LogEntry
+		if err := rows.Scan(&entry.id, &entry.createdAt, &entry.logType, &entry.logMsg); err != nil {
+			log.Fatal(err)
+		}
+		entries = append(entries, entry)
+	}
+	return entries
+}
+
+func (r *Repo) GetAll() []LogEntry {
+	sql := "SELECT * FROM dslog ORDER BY id ASC"
+	rows, err := r.db.Query(sql)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	entries := []LogEntry{}
+	for rows.Next() {
+		var entry LogEntry
+		if err := rows.Scan(&entry.id, &entry.createdAt, &entry.logType, &entry.logMsg); err != nil {
+			log.Fatal(err)
+		}
+		entries = append(entries, entry)
+	}
+	return entries
+}
+
+func main() {
+	var repo Repo
+	repo.Connect()
+	defer repo.Close()
+
+	for _, entry := range repo.GetById(12) {
+		entry.nicePrintln()
+	}
+
+	// fmt.Println(repo.GetById(12))
+
+	// fmt.Print(repo.Get(556677))
+
+	// rows := repo.GetByTimeRange(time.Now().Add(-time.Hour*5), time.Now())
+	// for _, v := range rows {
+	// 	v.nicePrintln()
+	// fmt.Printf("%d %s %s %s\n", v.id, v.createdAt.Format(time.RFC3339), v.logType, v.logMsg)
+	// fmt.Printf("%+v\n", v)
+	// }
+
+	// id, err := repo.Insert(time.Now(), "INFO", "Test Insert")
+	// if err != nil {
+	// 	log.Fatal("error inserting new item: ", err)
+	// 	return
+	// }
+
+	// entry, err := repo.Get(id)
+	// if err != nil {
+	// 	log.Fatal("error fetching item: ", entry, err)
+	// 	return
+	// }
+	// fmt.Printf("%+v\n", entry)
+}
